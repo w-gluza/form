35201b072dd519f14d9f1928a49b5d0fe7102e93
Wioletta Gluza <wiolagluza@gmail.com>
2020-03-31 15:33:56 +0200

    maping

--- src/components/ScheduleContainer.jsx
+++ src/components/ScheduleContainer.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ScheduleItem from "./ScheduleItem";
 
 const ScheduleContainer = ({ schedule }) => {
+  const datePlaceholder = "1970-01-01T";
+
   return (
     <div className="gridContainer">
       {schedule.map(day => (
         <ScheduleItem
           dayName={day.dayName}
           meals={day.meals}
-          startTime={day.startTime}
-          endTime={day.endTime}
+          startTime={`${datePlaceholder}${day.startTime}`}
+          endTime={`${datePlaceholder}${day.endTime}`}
+          // onChange={value => handleChange(key, value)}
         />
       ))}
     </div>
--- src/components/ScheduleItem.jsx
+++ src/components/ScheduleItem.jsx
@@ -1,12 +1,45 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import { KeyboardTimePicker } from "@material-ui/pickers";
 
-const ScheduleItem = ({ dayName, meals, startTime, endTime }) => {
+const ScheduleItem = ({ dayName, meals, startTime, endTime, onChange }) => {
+  const removeKeyboardStyles = { disabled: true, style: { display: "none" } };
+
+  const handleChange = (name, value) => {
+    // if (isDisabled) return;
+
+    onChange({
+      [name]: value
+    });
+  };
+
   return (
     <div className="grid">
       <h5>{dayName}</h5>
-      <p>{meals}</p>
-      <p>{startTime}</p>
-      <p>{endTime}</p>
+      <TextField
+        className="field"
+        defaultValue={meals}
+        id="standard-number"
+        type="number"
+        onChange={handleChange}
+      />
+
+      <KeyboardTimePicker
+        // clearable
+        ampm={false}
+        value={startTime}
+        // onChange={value => handleChange("startTime", value)}
+        KeyboardButtonProps={removeKeyboardStyles}
+      />
+
+      <KeyboardTimePicker
+        // clearable
+        ampm={false}
+        value={endTime}
+        // value={`${datePlaceholder}${endTime}`}
+        // onChange={value => handleChange("endTime", value)}
+        KeyboardButtonProps={removeKeyboardStyles}
+      />
     </div>
   );
 };
